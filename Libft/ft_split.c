015ba1ec19dab733c14200a099998938fe33a5b8
peacover <yousseferraki98@gmail.com>
2021-03-30 10:25:58 +0100

    init

---
+++ Libft/ft_split.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yer-raki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/15 16:54:53 by yer-raki          #+#    #+#             */
+/*   Updated: 2019/10/23 22:30:34 by yer-raki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		nb_w(char const *s, char c)
+{
+	int i;
+	int x;
+	int l;
+
+	i = 0;
+	x = 0;
+	l = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			x = 0;
+		else if (x == 0)
+		{
+			x = 1;
+			l++;
+		}
+		i++;
+	}
+	return (l);
+}
+
+int		nb_c(char const *s, char c, int i)
+{
+	int l;
+
+	l = 0;
+	while (s[i] != c && s[i] != '\0')
+	{
+		l++;
+		i++;
+	}
+	return (l);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**w;
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	j = 0;
+	if (!s)
+		return (NULL);
+	w = (char**)malloc(sizeof(char *) * (nb_w(s, c) + 1));
+	if (!w)
+		return (NULL);
+	while (s[i] != '\0' && nb_w(s, c) > j)
+	{
+		k = 0;
+		while (s[i] == c)
+			i++;
+		w[j] = (char *)malloc(sizeof(char) * (nb_c(s, c, i) + 1));
+		while (s[i] != c && s[i])
+			w[j][k++] = s[i++];
+		w[j][k] = '\0';
+		j++;
+	}
+	w[j] = 0;
+	return (w);
+}
--- Makefile
+++ Makefile
@@ -1,1 +1,19 @@
-test
+NAME = minishell
+LIBFT =	Libft/libft.a
+CC = gcc
+INC = -I includes/
+SRCS = ./srcs/main.c ./srcs/parsing/test.c
+FLAGS = -Wall -Wextra -Werror
+OBJS = $(SRC:.c=.o)
+all: $(NAME)
+$(NAME): $(OBJS) $(LIBFT)
+	@$(CC) $(FLAGS) $(INC) $(SRCS) -o $(NAME) $(LIBFT)
+$(LIBFT):
+	@make -C ./Libft
+clean:
+	@rm -f $(OBJS)
+	@make clean -C ./libft
+fclean: clean
+	@rm -f $(NAME)
+	@make fclean -C ./libft
+re : fclean $(NAME)
